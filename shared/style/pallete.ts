a11d178f5fd34d34dc0bcf5ca881bbdfed630316
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-07-08 17:26:39 -0300

    feat: pacote de estilos

---
+++ shared/style/pallete.ts
@@ -0,0 +1,12 @@
+export const pallete = {
+    light: {
+        "primary": "#0670A8",
+        "onPrimary": "#ffffff",
+        "primaryContainer": "#88C9EC",
+        "onPrimaryContainer": "#033D5D",
+        "secondary": "#78AACE",
+        "onSecondary": "#18374C",
+        "secondaryContainer": "#CCE6F9",
+        "onSecondaryContainer": "#3B6A8A",
+    } 
+}
